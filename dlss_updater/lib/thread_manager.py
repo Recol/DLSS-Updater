dff2805033e4eeede9323ab9282819a23e64401b
Alexis Schwindt <alexis.schwindt@outlook.com>
2024-11-26 16:33:47 -0300

    Revert "Added running guards to the thread  manager and ability to return output for called functions."
    This reverts commit 9ced9f03dd217a954817ca109cab5fbcb857380e.

--- dlss_updater/lib/thread_manager.py
+++ dlss_updater/lib/thread_manager.py
@@ -2,19 +2,15 @@ from PyQt6.QtCore import QThread, pyqtSignal
 
 class ThreadManager(QThread):
     finished = pyqtSignal()
-    result = pyqtSignal(object)
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.assigned_function = None
         self.args = None
         self.kwargs = None
-        self.is_running = False
 
     def assign_function(self, func, *args, **kwargs):
         """Assign a function and its arguments to be executed in the thread."""
-        if self.is_running:
-            return
         self.assigned_function = func
         self.args = args
         self.kwargs = kwargs
@@ -22,9 +18,5 @@ class ThreadManager(QThread):
     def run(self):
         """Execute the assigned function in the thread."""
         if self.assigned_function is not None:
-            self.is_running = True
-            function_output = self.assigned_function(*self.args, **self.kwargs)
-            if function_output is not None:
-                self.result.emit(function_output)
+            self.assigned_function(*self.args, **self.kwargs)
             self.finished.emit()
-            self.is_running = False
