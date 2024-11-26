39c407db4f85802c9817a6e34f6d014a0100bab7
Alexis Schwindt <alexis.schwindt@outlook.com>
2024-11-26 16:16:54 -0300

    Added a QRunnable custom object to execute arbitrary functions in threadpools

--- dlss_updater/lib/thread_manager.py
+++ dlss_updater/lib/thread_manager.py
@@ -1,21 +1,22 @@
-from PyQt6.QtCore import QThread, pyqtSignal
+from PyQt6.QtCore import QThreadPool, QRunnable, QObject, pyqtSignal
 
-class ThreadManager(QThread):
+class WorkerSignals(QObject):
     finished = pyqtSignal()
     result = pyqtSignal(object)
     error = pyqtSignal(object)
+
+
+class ThreadManager(QThreadPool):
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.assigned_function = None
         self.args = None
         self.kwargs = None
-        self.is_running = False
+        self.signals = WorkerSignals()
 
     def assign_function(self, func, *args, **kwargs):
         """Assign a function and its arguments to be executed in the thread."""
-        if self.is_running:
-            return
         self.assigned_function = func
         self.args = args
         self.kwargs = kwargs
@@ -24,12 +25,31 @@ class ThreadManager(QThread):
         """Execute the assigned function in the thread."""
         if self.assigned_function is not None:
             try:
-                self.is_running = True
                 function_output = self.assigned_function(*self.args, **self.kwargs)
                 if function_output is not None:
-                    self.result.emit(function_output)
+                    self.signals.result.emit(function_output)
             except Exception as e:
-                    self.error.emit(f'Exception during function execution: {e}')
+                self.signals.error.emit(f'Exception during function execution: {e}')
             finally:
-                self.finished.emit()
-                self.is_running = False
+                self.signals.finished.emit()
+
+
+class Worker(QRunnable):
+    def __init__(self, func, *args, **kwargs):
+        super().__init__()
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+    def run(self):
+        """Execute the assigned function and emit signals."""
+        if self.func is not None:
+            try:
+                function_output = self.func(*self.args, **self.kwargs)
+                if function_output is not None:
+                    self.signals.result.emit(function_output)  # Emit result signal
+            except Exception as e:
+                self.signals.error.emit(f'Exception during function execution: {e}')  # Emit error signal
+            finally:
+                self.signals.finished.emit()  # Emit finished signal
